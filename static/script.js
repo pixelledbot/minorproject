cf68b890267611e1d6a80586b275de5e39dc92f3
pixelledbot <simran2315222@gmail.com>
2026-04-11 12:48:18 +0530

    Add static files (CSS/JS)

---
+++ static/script.js
@@ -0,0 +1,7 @@
+function previewImage(event) {
+    const reader = new FileReader();
+    reader.onload = function(){
+        document.getElementById('preview').src = reader.result;
+    }
+    reader.readAsDataURL(event.target.files[0]);
+}
